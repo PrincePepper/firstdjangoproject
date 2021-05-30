d5e7e04838f641fc75f290e738eaf7b868ee7d42
semyon <semyon3336@gmail.com>
2021-05-30 15:23:59 +1000

    aaaa

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,4 @@ from django.template import RequestContext, Template
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'main_app/index.html')
